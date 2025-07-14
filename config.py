dee556d5d34b6cd75902582adfed7fc98d7c4a39
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-07-14 15:24:59 +0100

    Increase default relevance title match weight from 5 to 10

--- config.py
+++ config.py
@@ -103,7 +103,7 @@ class Base(object):
     )
 
     RELEVANCE_TITLE_MATCH_WEIGHT: float = float(
-        os.environ.get("RELEVANCE_TITLE_MATCH_WEIGHT", "5")
+        os.environ.get("RELEVANCE_TITLE_MATCH_WEIGHT", "10")
     )
     RELEVANCE_BODY_MATCH_WEIGHT: float = float(
         os.environ.get("RELEVANCE_BODY_MATCH_WEIGHT", "2")
