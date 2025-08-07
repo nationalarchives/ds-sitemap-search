88c7d162e3d1ca2f845bf3b8e4617bdefe93ba3e
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-08-07 15:05:00 +0100

    Revert "Add temporary healthcheck endpoint to verify service status"
    This reverts commit 83a15409d4b97fbd21f26b3ffef2d8214a0edf42.

--- app/sitemap_search/routes.py
+++ app/sitemap_search/routes.py
@@ -268,9 +268,3 @@ def index():
             results_per_page=results_per_page,
             pagination={},
         )
-
-
-@bp.route("/healthcheck/")
-def temp_healthcheck():
-    # This is a temporary healthcheck endpoint to ensure the service is running.
-    return "OK", 200
