7a0f454ce36314e7d25d324c06454318362b3a7b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-08-11 10:46:13 +0100

    Refactor main execution flow to remove exception handling and simplify argument checking

--- add_new.py
+++ add_new.py
@@ -3,9 +3,9 @@ import sys
 from populate import populate, process_sitemap
 
 if __name__ == "__main__":
-    try:
+    if len(sys.argv) > 1:
         sitemap = sys.argv[1]
         process_sitemap(sitemap=sitemap, skip_existing=True)
-    except IndexError:
+    else:
         populate(skip_existing=True)
     exit(0)
