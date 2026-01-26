87cb833cf63494177c0a2e316eab34fa7dc9e2d7
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-01-26 14:40:31 +0000

    Refactor URL handling to incorporate environment variable remaps and improve archived URL checks

--- app/lib/urls.py
+++ app/lib/urls.py
@@ -1,15 +1,33 @@
 from config import ARCHIVED_URLS, DOMAIN_REMAPS
-
+import json
+import os
 
 def correct_url(url):
-    for domain, replacement in DOMAIN_REMAPS.items():
+    remaps = DOMAIN_REMAPS | (
+        json.loads(os.environ.get("DOMAIN_REMAPS", "{}"))
+    )
+    for domain, replacement in remaps.items():
         if url.startswith(domain):
             return url.replace(domain, replacement)
     return url
 
 
 def is_url_archived(url):
-    for archived_url in ARCHIVED_URLS:
+    archived_urls: list[str] = [
+        domain
+        for domain in (
+            ARCHIVED_URLS
+            + [
+                archived_url
+                for archived_url in os.environ.get("ARCHIVED_URLS", "").split(
+                    ","
+                )
+                if archived_url
+            ]
+        )
+        if domain
+    ]
+    for archived_url in archived_urls:
         if url.startswith(archived_url):
             return True
     return False
